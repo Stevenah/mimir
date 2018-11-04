0ff22a20755885732954e8d2cd39f4cd55a127db
Steven Hicks <shicks1000@gmail.com>
2018-11-04 21:12:17 +0100

    migration

--- mimir/apps/core/models.py
+++ mimir/apps/core/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Application(models.Model):
     name = models.CharField(max_length=80)
-    banner = models.ImageField(upload_to='apps/banners')
+    banner = models.ImageField(upload_to='apps/banners', default=None)
     description = models.CharField(max_length=80)
